2c646bf63fc63883e20d45385f312074dfa3e1c3
Malte <malte.kluft@gmail.com>
2022-08-31 10:06:33 +0200

    feat!: add proper stylesheet imports

--- src/util/TailwindMixin.ts
+++ src/util/TailwindMixin.ts
@@ -1,10 +1,13 @@
+import stylesheetURL from "../styles/main.css?url";
+
 export const TW = <T extends LitMixin>(superClass: T): T =>
   class extends superClass {
     connectedCallback() {
       super.connectedCallback();
 
-      document.head.querySelectorAll("link[rel='stylesheet']").forEach((link) => {
-        this.shadowRoot.append(link.cloneNode());
-      });
+      const link = document.createElement("link");
+      link.rel = "stylesheet";
+      link.href = stylesheetURL;
+      this.shadowRoot.append(link);
     }
   };
