e6801b501bbef23047e83932aa18b50ac23b625f
Mafrans <malte.kluft@gmail.com>
2023-05-15 10:23:05 +0200

    fix: use style tag if adopted css isn't supported

--- src/util/TailwindMixin.ts
+++ src/util/TailwindMixin.ts
@@ -1,10 +1,37 @@
 import css from "../styles/main.css?inline";
 
+const supportsAdoptedCSS = Array.isArray(document.adoptedStyleSheets);
+
 export const TW = <T extends LitMixin>(superClass: T): T =>
-  class extends superClass {
+  class Mixin extends superClass {
+    static styleElement: HTMLStyleElement;
+
+    static stylesheet: CSSStyleSheet;
+
+    // Disable no-explicit-any because Mixin constructor parameters must have an any[] type
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    constructor(...args: any[]) {
+      super(...args);
+      void this.createStylesheet();
+    }
+
+    async createStylesheet() {
+      if (supportsAdoptedCSS) {
+        if (!Mixin.stylesheet) {
+          Mixin.stylesheet = new CSSStyleSheet();
+          await Mixin.stylesheet.replace(css);
+        }
+        this.shadowRoot.adoptedStyleSheets.push(Mixin.stylesheet);
+      } else {
+        if (!Mixin.styleElement) {
+          Mixin.styleElement = document.createElement("style");
+          Mixin.styleElement.textContent = css;
+        }
+        this.shadowRoot.append(Mixin.styleElement.cloneNode());
+      }
+    }
+
     async connectedCallback() {
       super.connectedCallback();
-      const stylesheet = await new CSSStyleSheet().replace(css);
-      this.shadowRoot.adoptedStyleSheets.push(stylesheet);
     }
   };
